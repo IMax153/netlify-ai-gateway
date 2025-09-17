73af05182635bdeb6d915bad3d00a1b43ff53b8b
Maxwell Brown <maxwellbrown1990@gmail.com>
2025-09-17 12:27:12 -0400

    add basic effect program

--- src/routes/gateway.ts
+++ src/routes/gateway.ts
@@ -1,11 +1,21 @@
+import { LanguageModel } from "@effect/ai"
+import { OpenAiClient, OpenAiLanguageModel } from "@effect/ai-openai"
+import { FetchHttpClient } from "@effect/platform"
 import { createServerFileRoute } from "@tanstack/react-start/server"
+import { Config, Effect, Layer } from "effect"
+
+const program = Effect.gen(function* () {
+	const response = yield* LanguageModel.generateText({
+		prompt: "Why is the sky blue?",
+	})
+	return new Response(response.text)
+}).pipe(Effect.provide(OpenAiLanguageModel.model("gpt-5-nano-2025-08-07")))
+
+const MainLayer = OpenAiClient.layerConfig({
+	apiUrl: Config.string("OPENAI_BASE_URL"),
+	apiKey: Config.redacted("OPENAI_API_KEY"),
+}).pipe(Layer.provide(FetchHttpClient.layer))
 
 export const ServerRoute = createServerFileRoute("/gateway").methods({
-	GET: async ({ request }) => {
-		console.log({ AI_GATEWAY_BASE_URL: process.env.AI_GATEWAY_BASE_URL })
-		console.log({ AI_GATEWAY_OPENAI_KEY: process.env.AI_GATEWAY_OPENAI_KEY })
-		console.log({ OPENAI_BASE_URL: process.env.OPENAI_BASE_URL })
-		console.log({ OPENAI_API_KEY: process.env.OPENAI_API_KEY })
-		return new Response("Hello, World!")
-	},
+	GET: () => program.pipe(Effect.provide(MainLayer), Effect.runPromise),
 })

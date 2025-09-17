0ea141f79f3b3d0452fd0ef0fe94351ec0aa7b75
Maxwell Brown <maxwellbrown1990@gmail.com>
2025-09-17 10:50:31 -0400

    update server fn logs

--- src/routes/gateway.ts
+++ src/routes/gateway.ts
@@ -2,10 +2,10 @@ import { createServerFileRoute } from "@tanstack/react-start/server"
 
 export const ServerRoute = createServerFileRoute("/gateway").methods({
 	GET: async ({ request }) => {
-		console.log(process.env.AI_GATEWAY_BASE_URL)
-		console.log(process.env.AI_GATEWAY_OPENAI_KEY)
-		console.log(process.env.OPENAI_BASE_URL)
-		console.log(process.env.OPENAI_API_KEY)
+		console.log({ AI_GATEWAY_BASE_URL: process.env.AI_GATEWAY_BASE_URL })
+		console.log({ AI_GATEWAY_OPENAI_KEY: process.env.AI_GATEWAY_OPENAI_KEY })
+		console.log({ OPENAI_BASE_URL: process.env.OPENAI_BASE_URL })
+		console.log({ OPENAI_API_KEY: process.env.OPENAI_API_KEY })
 		return new Response("Hello, World!")
 	},
 })
